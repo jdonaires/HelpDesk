a26d79d390467d326d363d273d1d6003db6ae62c
CristianHernandezVillo <hernandezvillocristian@gmail.com>
2018-09-22 15:33:21 -0500

    Estructura de tablas
    codigo de creacion de tablas

--- Fuentes SQL/Fuente_Tablas.sql
+++ Fuentes SQL/Fuente_Tablas.sql
@@ -8,6 +8,52 @@ CREATE TABLE HelpDesk_Area (
 
 )ENGINE = InnoDB;
 
+CREATE TABLE HelpDesk_Perfil(
+	IdPerfil		INT	PRIMARY KEY
+,	Descripcion		VARCHAR(150)
+,	FechaCrea		DATE
+,	FlgEliminado	CHAR(1)
+)ENGINE = InnoDB;
+
+CREATE TABLE  HelpDesk_AccionVista(
+	IdAccionVista	INT PRIMARY KEY
+,	IdPerfil		INT
+,	Descripcion		VARCHAR(300)
+,	FechaCrea		DATE
+,	FlgEliminado	CHAR(1)
+,	FOREIGN KEY (IdPerfil) REFERENCES HelpDesk_Perfil (IdPerfil)
+)ENGINE = InnoDB;
+
+
+CREATE TABLE HelpDesk_Usuario(
+	IdUsuario		INT PRIMARY KEY
+,	IdPerfil		INT
+,	IdArea			INT
+,	Nombre			VARCHAR(200)
+,	Apellidos		VARCHAR(200)
+,	Correo			VARCHAR(200)
+,	Contrasenia		VARCHAR(300)
+,	Estado			VARCHAR(300)
+,	NroCelular		VARCHAR(15)
+,	Confirmacion	VARCHAR(300)
+,	FechaCrea		DATE
+,	FlgEliminado	CHAR(1)
+,	FOREIGN KEY (IdPerfil) REFERENCES HelpDesk_Perfil(IdPerfil)
+,	FOREIGN KEY (IdArea) REFERENCES HelpDesk_Area(IdArea)
+)ENGINE = InnoDB;
+
+CREATE TABLE HelpDesk_PrivilegioUsuario(
+	IdPriviligeoUsuario	INT PRIMARY KEY
+,	IdAccionVista		INT
+,	IdUsuario			INT
+,	Asignado			CHAR(1)
+,	FechaCrea			DATE
+,	FlgEliminado		CHAR(1)
+,	FOREIGN KEY (IdUsuario) REFERENCES HelpDesk_Usuario(IdUsuario)
+,	FOREIGN KEY (IdAccionVista) REFERENCES HelpDesk_AccionVista(IdAccionVista)
+)ENGINE = InnoDB;
+	
+
 CREATE TABLE HelpDesk_Categoria(
 	IdCategoria		INT PRIMARY KEY
 ,	Tipo			VARCHAR(50)
@@ -20,12 +66,24 @@ CREATE TABLE HelpDesk_Problema (
 	IdProblema		INT PRIMARY KEY
 ,	IdCategoria		INT
 ,	Descripcion		VARCHAR(150)
+,	Prioridad		VARCHAR(150)
+,	FechaEstimacion	DATE
 , 	FechaCrea		DATE
 ,	FlgElminado		CHAR(1)
 , 	FOREIGN KEY (IdCategoria) REFERENCES HelpDesk_Categoria (IdCategoria)
 )ENGINE = InnoDB;
 
+CREATE TABLE HelpDesk_Ticket(
+	IdTicket		INT
+,	IdCliente		INT
+,	IdProblema		INT
+,	Asunto			VARCHAR(450)
+,	Descripcion		TEXT
+,	FechaCrea		DATE
+,	FlgEliminado	CHAR(1)
+) ENGINE= InnoDB;
 
-drop table problema
-drop table Categoria
-drop table Area
+
+drop table HelpDesk_Problema;
+drop table HelpDesk_Categoria;
+drop table HelpDesk_Area;
