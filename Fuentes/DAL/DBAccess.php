b8fa75c721ba6313c144d74e5e295ce2e62f4782
lseNavarroSaravia <lsnavarro65@gmail.com>
2018-10-19 23:36:20 -0500

    Update Fuentes/DAL/DBAccess.php

--- Fuentes/DAL/DBAccess.php
+++ Fuentes/DAL/DBAccess.php
@@ -5,19 +5,19 @@ class DBAccess
   public function __construct()
   {
     try {
-<<<<<<< HEAD
+
 
     /*Conexion a bd actual*/
     /*$this->conn = new PDO('mysql:host=localhost:3306;dbname=helpdesk_2018', 'root', 'root');*/
 
     /* Conexion a bd LSNS*/
-     $this->conn = new PDO('mysql:host=localhost:3306;dbname=helpdesk_2018', 'root', '');
+     /*$this->conn = new PDO('mysql:host=localhost:3306;dbname=helpdesk_2018', 'root', '');*/
 
 
 
-=======
+
       $this->conn = new PDO('mysql:host=db4free.net:3306;dbname=helpdesk_2018', 'usert_test', '');
->>>>>>> 88bbafda3d3ead49b9ecdd42b45897d6cf65692e
+
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $ex ) {
       echo "error:" .$ex->getMessage();
