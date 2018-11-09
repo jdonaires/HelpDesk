ed5fad7296f9efdf7ca49e2265a3ef69aaf3975f
AParejaA <daia.15170@gmail.com>
2018-11-09 00:44:59 -0500

    Update Fuentes/DAL/DBAccess.php
    t_d

--- Fuentes/DAL/DBAccess.php
+++ Fuentes/DAL/DBAccess.php
@@ -8,11 +8,11 @@ class DBAccess
 
 
     /*Conexion a bd actual*/
-<<<<<<< HEAD
+
   //  $this->conn = new PDO('mysql:host=localhost:3306;dbname=helpdesk_2018', 'root', 'root');
-=======
+
     //$this->conn = new PDO('mysql:host=localhost:3306;dbname=helpdesk_2018', 'root', 'root');
->>>>>>> a1e00df8e0a5426d15172fd229fce114cbf068ba
+
 
     /* Conexion a bd LSNS*/
     // $this->conn = new PDO('mysql:host=localhost:3306;dbname=helpdesk_2018', 'root', '');
@@ -22,18 +22,18 @@ class DBAccess
 
     $this->conn = new PDO('mysql:host=db4free.net:3306;dbname=helpdesk_2018', 'usert_test', '');
 
-<<<<<<< HEAD
+
 
 
   /*   $this->conn = new PDO('mysql:host=db4free.net:3306;dbname=helpdesk_2018', 'usert_test', '');
 */
 
-=======
+
      $this->conn = new PDO('mysql:host=db4free.net:3306;dbname=helpdesk_2018', 'usert_test', '');
 
   /*   $this->conn = new PDO('mysql:host=db4free.net:3306;dbname=helpdesk_2018', 'usert_test', '');
 */
->>>>>>> a1e00df8e0a5426d15172fd229fce114cbf068ba
+
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $ex ) {
       echo "error:" .$ex->getMessage();
