349f0d9e60dfb52e2cd42ff14d779784e7585181
kflorespa <niickcreew@gmail.com>
2018-11-09 09:19:44 -0500

    cambios en diseño
    se cambio el diseño de las vistas del administrador

--- Fuentes/DESIGNER/a-contadmin.php
+++ Fuentes/DESIGNER/a-contadmin.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Dashboard | HelpDesk</title>
+      <title>HelpDesk | Lista de administradores</title>
 	<link rel="stylesheet" href="css/tabla.css">
     <!-- Bootstrap -->
     <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -37,7 +37,7 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-info-circle"></i> <span>HelpDesk!</span></a>
+              <a href="a-bentrada.php" class="site_title"><i class="fa fa-paper-plane-o"></i> <span>HelpDesk</span></a>
             </div>
 
             <div class="clearfix"></div>
@@ -45,7 +45,7 @@
             <!-- menu profile quick info -->
             <div class="profile clearfix">
               <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+                <img src="images/user.png" alt="..." class="img-circle profile_img">
               </div>
               <div class="profile_info">
                 <span>Bienvenido,</span>
@@ -64,6 +64,17 @@
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
+                  <li><a><i class="fa fa-envelope"></i>Bandeja<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="a-bentrada.php">Entrada</a></li>
+                      <li><a href="a-bproceso.php">Proceso</a></li>
+                      <li><a href="a-bvalidez.php">Validez</a></li>
+                      <li><a href="a-bsalida.php">Salida</a></li>
+                    </ul>
+                  </li></ul>
+
+
+                <ul class="nav side-menu">
                   <li><a><i class="fa fa-user"></i> Administrar Datos <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="a-contnuevoc.php">Control Nuevos</a></li>
@@ -71,47 +82,30 @@
                       <li><a href="a-contcliente.php">Control Clientes</a></li>
                       <li><a href="a-contti.php">Control Soporte TI</a></li>
                     </ul>
-                  </li>
-                  <ul class="nav side-menu">
-                    <li><a><i class="fa fa-inbox"></i>Bandeja<span class="fa fa-chevron-down"></span></a>
-                      <ul class="nav child_menu">
-                        <li><a href="a-bentrada.php">Entrada</a></li>
-                        <li><a href="a-bproceso.php">Proceso</a></li>
-                        <li><a href="a-bvalidez.php">Validez</a></li>
-                        <li><a href="a-bsalida.php">Salida</a></li>
-                      </ul>
-                    </li>
+                  </li></ul>
+
                     <ul class="nav side-menu">
-                      <li><a><i class="fa fa-home"></i>Dashboard<span class="fa fa-chevron-down"></span></a>
+                      <li><a><i class="fa fa-dashboard"></i>Dashboard<span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                          <li><a href="a-bentrada.php">Entrada</a></li>
+                          <li><a href="#">Abrir</a></li>
                         </ul>
-                        <a href="https://dashboard.smartsupp.com/v2" target="_blank" onclick="window.open(this.href,this.target,"width=400,height=150,top=200,left=200,toolbar=no,location=no,status=no,menubar=no");return false;">Mensajeria<span> </a>
+
                       </li>
-
                 </ul>
-              </div>
 
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-comment"></i>Contacto<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li>  <a href="https://dashboard.smartsupp.com/v2" target="_blank" onclick="window.open(this.href,this.target,"width=400,height=150,top=200,left=200,toolbar=no,location=no,status=no,menubar=no");return false;">Mensajeria<span> </a>
+                    </li>
+                    </ul>
+                  </li>
+                    </ul>
+
+              </div>
 
             </div>
             <!-- /sidebar menu -->
-
-            <!-- /menu footer buttons -->
-            <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
-            </div>
-            <!-- /menu footer buttons -->
           </div>
         </div>
 
@@ -126,23 +120,16 @@
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">
+                          <img src="images/user.png" alt="">
                     <?php
       								echo($_SESSION["UsuarioLogin"][0]["Nombre"]);
       							?>
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Perfil</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Configuracion</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Ayuda
-                    </a></li>
-                    <li><a href="salir.php"><i class="fa fa-sign-out pull-right"></i> Cerrar Sesion</a></li>
+                    <li><a href="a-miperfil.php">Mi perfil</a></li>
+                    <li><a href="#">Cambiar de contraseña</a></li>
+                  <li><a href="salir.php"><i class="fa fa-sign-out pull-right"></i> Cerrar Sesion</a></li>
                   </ul>
                 </li>
 
@@ -156,60 +143,61 @@
 
         <!-- page content -->
         <div class="right_col" role="main">
-          <!-- top tiles -->
-          <form role="form"  action="../pag/ver.php" method="POST">
-            <table class="table-fill" >
-              <div class="alert alert-info">
-          </div>
-            <h2>Control Soporte Tecnico</h2>
-          </br>
-          </br>
-          <thead>
-              <tr>
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                      <div class="x_panel">
+                        <div class="x_title">
+                          <h2>Lista de administradores</h2>
+                          <div class="clearfix"></div>
+                        </div>
+                        <div class="x_content">
 
+                          <form role="form"  action="../pag/ver.php" method="POST">
+                          <table class="table">
+                            <thead>
+                              <tr>
+                                <th class="text-left">Nombre completo</th>
+                                <th class="text-left">Area</th>
+                                <th class="text-left">Perfil</th>
+                                <th class="text-left">Celular</th>
+                                <th class="text-left">Email</th>
+                                <th class="text-left">Estado</th>
+                                <th class="text-left">Revisar</th>
+                              </tr>
+                            </thead>
+                            <?php
+                            require_once '..\DAL\DBAccess.php';
+                            $dba = new DBAccess();
+                            $conn = $dba->Get_Connection();
+                            $stmt = $conn->prepare("call spHelpDesk_GET_ContUsuarios(3);");
+                            $stmt->execute();
+                            while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+                            echo'
+                                  <tbody class="table-hover">
+                                    <tr>
+                                    <td>'.$row->Nombre.' '.$row->Apellidos.'</td>
+                                    <td>'.$row->Area.'</td>
+                                    <td>'.$row->Perfil.'</td>
+                                    <td>'.$row->NroCelular.'</td>
+                                    <td>'.$row->Correo.'</td>
+                                    <td>'.$row->Estado.'</td>
+                                    <td><a href="a-detalleuser.php?IdUsuario='.$row->IdUsuario.'" target="_blank" onclick="window.open(this.href,this.target,"width=400,height=150,top=200,left=200,toolbar=no,location=no,status=no,menubar=no");return false;"><span class="fa fa-eye"> </a></td>
+                                  </tr>
+                                  </tbody>';
+                                  }
+                                  ?>
+                          </table>
+   </form>
+                        </div>
+                      </div>
+                    </div></div>
 
-  							<th class="text-left">Nombre completo</th>
-  							<th class="text-left">Area</th>
-  							<th class="text-left">Perfil</th>
-  							<th class="text-left">Celular</th>
-  							<th class="text-left">Email</th>
-  							<th class="text-left">Estado</th>
-  							<th class="text-left">Revisar</th>
-              </tr>
-                </thead>
-          <?php
-          require_once '..\DAL\DBAccess.php';
-          $dba = new DBAccess();
-          $conn = $dba->Get_Connection();
-          $stmt = $conn->prepare("call spHelpDesk_GET_ContUsuarios(3);");
-          $stmt->execute();
-          while($row = $stmt->fetch(PDO::FETCH_OBJ)){
-          echo'
-                <tbody class="table-hover">
-                  <tr>
-                  <td>'.$row->Nombre.' '.$row->Apellidos.'</td>
-                  <td>'.$row->Area.'</td>
-                  <td>'.$row->Perfil.'</td>
-                  <td>'.$row->NroCelular.'</td>
-                  <td>'.$row->Correo.'</td>
-                  <td>'.$row->Estado.'</td>
-                  <td><a href="a-detalleuser.php?IdUsuario='.$row->IdUsuario.'" target="_blank" onclick="window.open(this.href,this.target,"width=400,height=150,top=200,left=200,toolbar=no,location=no,status=no,menubar=no");return false;"><span class="fa fa-eye"> </a></td>
-                </tr>
-                </tbody>';
-                }
-                ?>
-              </table>
-                   </form>
-
-              </div>
+                    </div>
             </div>
           </div>
         </div>
 
         <footer>
-          <div class="pull-right">
-            HelpDesk - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-          </div>
           <div class="clearfix"></div>
         </footer>
 
