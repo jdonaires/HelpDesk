431ea04dd5a7ac9ba6ce797d7b00b97bc632a6f3
AndersRo <anroqui2011@gmail.com>
2018-11-10 04:39:34 -0500

    sql

--- SQL/PROCEDURE/spHelpDesk_GET_Ticket.sql
+++ SQL/PROCEDURE/spHelpDesk_GET_Ticket.sql
@@ -79,8 +79,8 @@ END //
 
 
 delimiter //
-CREATE PROCEDURE spHelpDesk_View_TicUs(
-	
+CREATE PROCEDURE spHelpDesk_View_DetTicket(
+	in _IdTicket INT
 )
 BEGIN
 SELECT
@@ -101,7 +101,7 @@ SELECT
 	INNER JOIN HelpDesk_Area as ARE on ARE.IdArea = US.IdArea
 	INNER JOIN HelpDesk_Problema as PRO ON PRO.IdProblema = TIC.IdProblema
 	INNER JOIN HelpDesk_Categoria as CAT ON CAT.IdCategoria = PRO.IdCategoria
-	where US.IdUsuario;
+	where TIC.IdTicket=_IdTicket;
 END //
 
 DELIMITER //
@@ -136,4 +136,27 @@ SELECT
     INNER JOIN HelpDesk_Area as ARE ON ARE.IdArea = US.IdArea
 	INNER JOIN HelpDesk_Problema as PRO ON PRO.IdProblema = TIC.IdProblema
 	where TIC.IdTicket;
+END //
+
+delimiter //
+CREATE PROCEDURE spHelpDesk_Det_AsigTicket(
+	in _IdTicket INT
+)
+BEGIN
+	SELECT
+		TIC.IdTicket,
+		CAT.Tipo,
+		PRO.Descripcion as Problema,
+		TIC.Descripcion,
+		ARE.Descripcion as Area,
+		TIC.FechaCrea,
+		PRO.Prioridad,
+		PRO.FechaEstimacion,
+			concat(CAT.Tipo," ",PRO.Descripcion) AS Asunto
+		FROM HelpDesk_Ticket as TIC
+		INNER JOIN HelpDesk_Usuario as US ON US.IdUsuario = TIC.IdCliente
+		INNER JOIN HelpDesk_Area as ARE on ARE.IdArea = US.IdArea
+		INNER JOIN HelpDesk_Problema as PRO ON PRO.IdProblema = TIC.IdProblema
+		INNER JOIN HelpDesk_Categoria as CAT ON CAT.IdCategoria = PRO.IdCategoria
+		where TIC.IdTicket=_IdTicket;
 END //
