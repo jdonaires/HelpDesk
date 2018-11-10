517f523eb061571aa70d8239712b8533a77c7dab
AndersRo <anroqui2011@gmail.com>
2018-11-10 04:11:04 -0500

    sp

--- SQL/PROCEDURE/spHelpDesk_GET_Ticket.sql
+++ SQL/PROCEDURE/spHelpDesk_GET_Ticket.sql
@@ -3,6 +3,7 @@ drop procedure spHelpDesk_GET_DetTicket;
 drop procedure spHelpDesk_UP_DetTicket;
 drop procedure spHelpDesk_SET_DetTicket;
 drop procedure spHelpDesk_GET_Ticket;
+drop procedure spHelpDesk_View_Ticket;
 
 
 DELIMITER $$
@@ -77,6 +78,32 @@ SELECT
 END //
 
 
+delimiter //
+CREATE PROCEDURE spHelpDesk_View_TicUs(
+	
+)
+BEGIN
+SELECT
+	TIC.IdTicket,
+    CAT.Tipo,
+    TID.IdTicketDetalle,
+	PRO.Descripcion,
+    TIC.Descripcion as TDes,
+	ARE.Descripcion as Area,
+	TID.Estado,
+	TIC.FechaCrea,
+	PRO.Prioridad,
+	PRO.FechaEstimacion,
+		concat(CAT.Tipo," ",PRO.Descripcion) AS Asunto
+	FROM HelpDesk_Ticket as TIC
+	INNER JOIN HelpDesk_TicketDetalle as TID ON TID.IdTicket= TIC.IdTicket
+	INNER JOIN HelpDesk_Usuario as US ON US.IdUsuario = TID.IdResponsable
+	INNER JOIN HelpDesk_Area as ARE on ARE.IdArea = US.IdArea
+	INNER JOIN HelpDesk_Problema as PRO ON PRO.IdProblema = TIC.IdProblema
+	INNER JOIN HelpDesk_Categoria as CAT ON CAT.IdCategoria = PRO.IdCategoria
+	where US.IdUsuario;
+END //
+
 DELIMITER //
 CREATE PROCEDURE spHelpDesk_UP_DetTicket(
 	in _IdTicketDetalle INT,
@@ -87,4 +114,26 @@ UPDATE HelpDesk_TicketDetalle
     SET
     IdResponsable=_Responsable
 	WHERE IdTicketDetalle  =  _IdTicketDetalle;
+END //
+
+
+DELIMITER //
+CREATE PROCEDURE spHelpDesk_View_Ticket(
+	
+)
+BEGIN
+SELECT
+	TIC.IdTicket,
+    US.Nombre as Cliente,
+    ARE.Descripcion as Area,
+	PRO.Descripcion as Problema,
+    TIC.Asunto,
+    PRO.Prioridad,
+    TIC.Descripcion as Descripcion,
+	TIC.FechaCrea
+	FROM HelpDesk_Ticket as TIC
+	INNER JOIN HelpDesk_Usuario as US ON US.IdUsuario = TIC.IdCliente
+    INNER JOIN HelpDesk_Area as ARE ON ARE.IdArea = US.IdArea
+	INNER JOIN HelpDesk_Problema as PRO ON PRO.IdProblema = TIC.IdProblema
+	where TIC.IdTicket;
 END //
