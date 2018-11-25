38c0068ea218e8836c861beaeed9d784a0cbd65f
Paulcastilla <jhampiercastilla@outlook.com>
2018-11-24 21:42:52 -0500

    intento de reporte
    intento de reporte :-1:

--- Desarrollo/DESIGNER/DashBoard.php
+++ Desarrollo/DESIGNER/DashBoard.php
@@ -7,6 +7,7 @@
     </head>
     <body>
         <div><canvas  id="Grafico"  class="chart" width="1000px" height="200"></canvas></div>
+        <div><canvas  id="Grafico2"  class="chart" width="1000px" height="200"></canvas></div>
     </body>
 </html>
 <script>
@@ -59,13 +60,56 @@
         }
 });
     });
-</script>
- <style>
-        
-        .resultados{
-            margin: auto;
-            margin: 100px;
-            width: 1000px;
+
+
+$(document).ready(function(){
+        var contexto = $("#Grafico").get(0).getContext("2d"); 
+        new Chart(contexto, {
+        type: 'bar',
+        fillOpacity : 100,  
+        data: {
+                labels : ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+                datasets : [{
+                                label: "demo1",
+                                fill: true,
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.2)',
+                                ], //COLOR DE LAS BARRAS
+                                borderColor: 'rgba(255, 99, 132, 0.2)', //COLOR DEL BORDE DE LAS BARRAS
+                                data : [3,4,5,6,7,7,7,8,7,8,7,7],
+                                borderWidth: 1,
+                                opacity:0,
+                        },
+                            {
+                                    label: "demo16778",
+                                    backgroundColor: [
+                                        'rgba(0, 99, 145, 0.2)',
+                                    ], //COLOR DE LAS BARRAS
+                                    borderColor: 'rgba(0, 99, 145, 0.2)', //COLOR DEL BORDE DE LAS BARRAS
+                                    data : [3,2,4,5,71,8,9,0,2,3,4],
+                                    borderWidth: 1,
+                                    opacity:0,
+                            }]
+        },
+        options: {
+            display :false,
+                reponsive:true,
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: false,
+                    text: 'Demo Help Desk'
+                },
+                scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+                }
         }
-        
-    </style>
+	});
+    });
+
+</script>
